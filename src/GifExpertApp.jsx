38e6f2002b85bc32241fa8ee7e2d5c18d3bcd9b4
naranjo <naranjo.developer@gmail.com>
2024-01-10 16:40:37 -0500

    v1

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,15 @@
+function GifExpertApp() {
+  return (
+    <>
+      {/**TITULO */}
+      <h1 className="titulo">Gif Expert App</h1>
+
+      {/**TITULO */}
+
+      {/*Listado De Gifs */}
+      {/*GifItem */}
+    </>
+  );
+}
+
+export default GifExpertApp;
