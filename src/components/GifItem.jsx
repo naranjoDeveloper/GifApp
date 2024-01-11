b05dd39418b71d269cfa1f8d6ffc6fa6712e1291
naranjo <naranjo.developer@gmail.com>
2024-01-11 00:20:46 -0500

    tooltip añadido al hacer hover

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,4 +1,5 @@
 import Swal from "sweetalert2";
+import { Tooltip } from "react-tooltip";
 
 export default function GifItem({ title, url }) {
   const handleClick = () => {
@@ -15,7 +16,13 @@ export default function GifItem({ title, url }) {
 
   return (
     <>
-      <div className="card" onClick={handleClick}>
+      <div
+        className="card"
+        onClick={handleClick}
+        data-tooltip-id="my-tooltip"
+        data-tooltip-content="Click Para Copiar El URL del GIF!"
+      >
+        <Tooltip id="my-tooltip" />
         <p>{title}</p>
         <img src={url} alt="Img GIFS" />
       </div>
